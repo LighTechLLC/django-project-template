a0e52f1bfa19c36a68fd1eb03ad1d0de3924b793
Eugene Hatsko <eugene.hatsko@m.thelightech.com>
2023-05-18 17:11:49 +0300

    `pytest` added

---
+++ src/apps/users/tests/test_models.py
@@ -0,0 +1,22 @@
+import pytest
+from django.db import IntegrityError
+
+from apps.users.models import User
+
+
+@pytest.mark.django_db
+class TestUser:
+    def test_same_email_uniqueness(self):
+        email = "test@example.com"
+        User.objects.create_user(email=email, password="pass")
+        with pytest.raises(IntegrityError):
+            User.objects.create_user(email=email, password="pass")
+
+    def test_case_insensitive_email_uniqueness(self):
+        email = "test@example.com"
+        email1 = email.lower()
+        email2 = email.upper()
+        assert email1 != email2
+        User.objects.create_user(email=email1, password="pass")
+        with pytest.raises(IntegrityError):
+            User.objects.create_user(email=email2, password="pass")
