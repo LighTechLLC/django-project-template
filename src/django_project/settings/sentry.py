22b40f769f56352a09c22e6e0e5ba5f71a549010
Eugene Hatsko <eugene.hatsko@m.thelightech.com>
2023-07-21 23:38:37 +0300

    feature: `auto_session_tracking` moved in `config.toml`

--- src/django_project/settings/sentry.py
+++ src/django_project/settings/sentry.py
@@ -5,7 +5,7 @@ if SENTRY_DSN := config["sentry"]["dsn"]:
     sentry_sdk.init(
         dsn=SENTRY_DSN,
         integrations=[DjangoIntegration()],
-        auto_session_tracking=False,
+        auto_session_tracking=config["sentry"]["auto_session_tracking"],
         traces_sample_rate=0.01,
         environment=ENVIRONMENT,
     )
