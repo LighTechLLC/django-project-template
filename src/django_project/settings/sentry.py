bb8f4d0feebcf34412bcc5d49cee868670202142
Eugene Hatsko <eugene.hatsko@m.thelightech.com>
2023-09-19 04:22:34 +0300

    feature(settings): sentry updated by adding `traces_sample_rate` on config.toml level

--- src/django_project/settings/sentry.py
+++ src/django_project/settings/sentry.py
@@ -1,11 +1,12 @@
 import sentry_sdk
+from sentry_sdk.integrations.celery import CeleryIntegration
 from sentry_sdk.integrations.django import DjangoIntegration
 
 if SENTRY_DSN := config["sentry"]["dsn"]:
     sentry_sdk.init(
         dsn=SENTRY_DSN,
-        integrations=[DjangoIntegration()],
+        integrations=[DjangoIntegration(), CeleryIntegration()],
         auto_session_tracking=config["sentry"]["auto_session_tracking"],
-        traces_sample_rate=0.01,
+        traces_sample_rate=config["sentry"]["traces_sample_rate"],
         environment=ENVIRONMENT,
     )
