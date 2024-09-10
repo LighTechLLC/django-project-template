7c45b2fe285650646ceb7e923933416732b68dd3
Eugene Hatsko <eugene.hatsko@m.thelightech.com>
2024-09-10 19:51:51 +0300

    feature(packages): swagger conf updated & rest auth urls added on root urls level

--- src/django_project/urls.py
+++ src/django_project/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -11,4 +11,15 @@ if settings.ENVIRONMENT == settings.ENVIRONMENT_LOCAL:
     # enabling media files serving for local env
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+
+
+if settings.ENVIRONMENT not in [
+    settings.ENVIRONMENT_PROD,
+    settings.ENVIRONMENT_STAGE,
+]:
+    # enabling drf auth for swagger in case it's not stage/production env
+    urlpatterns += static(
+        "rest-auth/",
+        include("rest_framework.urls", namespace="rest_framework"),
     )
